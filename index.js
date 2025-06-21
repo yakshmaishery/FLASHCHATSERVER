3a80e3a5738670077baf8dfc96fb6edf17620f4c
yakshmaishery <yakshmaishery@users.noreply.github.com>
2025-06-21 15:13:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
+
+const server = http.createServer(app);
+
+// Create Socket.IO server with CORS enabled
+const io = new Server(server, {
+   cors: {
+      origin: '*', // Allow all origins. You can restrict to specific domains
+      methods: ['GET', 'POST']
+   }
+});
+
+// Listen for client connections
+io.on('connection', (socket) => {
+   //   console.log('A user connected:', socket.id);
+   socket.on('CreateConnection', (data) => {
+      io.emit('CreateConnection_BROADCAST', data); // broadcast message to all clients
+   });
+
+   socket.on('CreateConnection_CallbackServer', (data) => {
+      io.emit('CreateConnection_Callback', data); // broadcast message to all clients
+   });
+
+   socket.on('LeaveConnection', (data) => {
+      io.emit('LeaveConnection_BROADCAST', data); // broadcast message to all clients
+   });
+
+   socket.on('SocketChat', (data) => {
+      io.emit('SocketChat_BROADCAST', data); // broadcast message to all clients
+   });
+
+   socket.on("startFileTransfer", ({ type, name, size, AnotherID }) => {
+      socket.broadcast.emit("startFileTransferAnother", { type, name, size, AnotherID })
+   })
+   socket.on("chunkFileTransfer", ({ type, name, size, data, offset, AnotherID }) => {
+      socket.broadcast.emit("chunkFileTransferAnother", { type, name, size, data, offset, AnotherID })
+   })
+   socket.on("endFileTransfer", ({ type, name, AnotherID }) => {
+      socket.broadcast.emit("endFileTransferAnother", { type, name, AnotherID })
+   })
+   socket.on("endFileTransferAnotherCALLBACK", ({ msg, UserID, AnotherID }) => {
+      socket.broadcast.emit("endFileTransferUserCALLBACK", { msg, UserID, AnotherID })
+   })
+
+   socket.on('disconnect', () => {
+      //  console.log('User disconnected:', socket.id);
+   });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+   console.log(`Socket.IO server running at http://localhost:${PORT}/`);
+});
